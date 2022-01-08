9afa0c562bc1ea0690130ad5de3fc14af4804062
nikitenko <nikitenko.max@gmail.com>
2022-01-08 08:47:56 +0200

    28-get-notifications

--- client/src/redux/actions/notifyAction.js
+++ client/src/redux/actions/notifyAction.js
@@ -1,12 +1,16 @@
 import { TYPES } from './_types';
-import { postDataAPI, deleteDataAPI } from '../../utils/fetchData';
+import { postDataAPI, deleteDataAPI, getDataAPI } from '../../utils/fetchData';
+
+export const NOTIFY_TYPES = {
+  GET_NOTIFIES: 'GET_NOTIFIES',
+};
 
 export const createNotify =
   ({ msg, auth, socket }) =>
   async (dispatch) => {
     try {
       const res = await postDataAPI('notify', msg, auth.token);
-      console.log(res.data);
+      console.log(res);
       // Socket
       //   socket.emit('createPost', msg);
     } catch (err) {
@@ -21,7 +25,11 @@ export const removeNotify =
   ({ msg, auth, socket }) =>
   async (dispatch) => {
     try {
-      await deleteDataAPI(`notify/${msg.id}?url=${msg.url}`, auth.token);
+      const res = await deleteDataAPI(
+        `notify/${msg.id}?url=${msg.url}`,
+        auth.token
+      );
+      console.log(res);
       // Socket
       //   socket.emit('createPost', msg);
     } catch (err) {
@@ -31,3 +39,16 @@ export const removeNotify =
       });
     }
   };
+
+export const getNotifies = (token) => async (dispatch) => {
+  try {
+    const res = await getDataAPI('notify', token);
+
+    dispatch({ type: NOTIFY_TYPES.GET_NOTIFIES, payload: res.data.notifies });
+  } catch (err) {
+    dispatch({
+      type: TYPES.ALERT,
+      payload: { error: err.response.data.msg },
+    });
+  }
+};
--- server/controllers/notifyCtrl.js
+++ server/controllers/notifyCtrl.js
@@ -1,11 +1,10 @@
-const Notify = require('../models/Notify');
+const Notifies = require('../models/Notify');
 
 const notifyCtrl = {
   createNotify: async (req, res) => {
     try {
       const { id, recipients, url, text, content, image } = req.body;
-      const notify = new Notify({
-        // user: { type: mongoose.Types.ObjectId, ref: 'user' },
+      const notify = new Notifies({
         user: req.user._id,
         id,
         recipients,
@@ -23,12 +22,24 @@ const notifyCtrl = {
 
   removeNotify: async (req, res) => {
     try {
-      const notify = await Notify.findOneAndDelete({
+      const notify = await Notifies.findOneAndDelete({
         id: req.params.id,
         url: req.query.url,
       });
 
       return res.json({ notify });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+
+  getNotifies: async (req, res) => {
+    try {
+      const notifies = await Notifies.find({ recipients: req.user._id })
+        .sort('isRead')
+        .populate('user', 'avatar username');
+
+      return res.json({ notifies });
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
